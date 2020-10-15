d1a907557381a03c21b82960a07c8a7d12dbdefa
Elena <esanchez6@gmail.com>
2020-10-16 00:29:02 +0200

    añadido y centrados personajes

--- js/character.js
+++ js/character.js
@@ -1,1 +1,12 @@
-let character = {};
+class character {
+    constructor(name,attack,defense,life,speed,quote){
+        this.name = name;
+        this.attack = attack;
+        this.defense = defense;
+        this.life = life;  //vamos a hacer que cada uno tenga su vida, por eso no =100
+        this.speed = speed;
+        this.quote = quote;
+
+    }
+
+}
